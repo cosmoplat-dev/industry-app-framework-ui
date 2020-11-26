49ff9db29f48861a35550007af6423534ea8481b
wyq19940213 <65220944+wyq19940213@users.noreply.github.com>
2020-11-26 17:28:45 +0800

    Update validate.js
    添加校验方法

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -19,3 +19,13 @@ export function validUsername(str) {
   // const valid_map = ['admin', 'editor']
   // return valid_map.indexOf(str.trim()) >= 0
 }
+
+/** 验证字符是否全都为空格
+ * @param {String} str
+ */
+export function isAllEmpty (str) {
+  if (str && str.length > 0 && str.split(' ').join('').length === 0) {
+    return false
+  }
+  return true
+}
