ce3e9338cac6111e142e263626b130a611011743
zhangcongcong <zhangcongcong@haier.com>
2020-11-27 14:37:48 +0800

    修改：路由名称

--- src/router/index.js
+++ src/router/index.js
@@ -66,7 +66,7 @@ export const constantRoutes = [
         path: 'table',
         name: 'Table',
         component: () => import('@/views/table/index'),
-        meta: { title: 'Table', icon: 'table' }
+        meta: { title: '设备', icon: 'table' }
       }
     ]
   },
