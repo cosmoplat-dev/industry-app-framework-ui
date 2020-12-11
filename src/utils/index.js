a2c67fbac585bfb11da4515cae6c4db7ff8f0cbc
wyq19940213 <65220944+wyq19940213@users.noreply.github.com>
2020-12-11 16:42:58 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -151,3 +151,59 @@ export function removeClass(ele, cls) {
     ele.className = ele.className.replace(reg, ' ')
   }
 }
+
+/**
+ * @description: 计算时间差
+ * @param {Date} time 需要计算的时间
+ * @param {Date} nowTime 当前时间
+ * @return: 时间差字符串
+ * @author: mayako
+ */
+export function get_time_diff(time, nowTime) {
+  var diff = ''
+  if (!nowTime) {
+    nowTime = new Date().getTime()
+  }
+  var time_diff = nowTime - time
+  // 计算相差天数
+  var days = Math.floor(time_diff / (24 * 3600 * 1000))
+  if (days > 0) {
+    diff += days + '天'
+  }
+  // 计算相差小时数
+  var leave1 = time_diff % (24 * 3600 * 1000)
+  var hours = Math.floor(leave1 / (3600 * 1000))
+  if (hours > 0) {
+    diff += hours + '小时'
+  } else {
+    if (diff !== '') {
+      diff += hours + '小时'
+    }
+  }
+  // 计算相差分钟数
+  var leave2 = leave1 % (3600 * 1000)
+  var minutes = Math.floor(leave2 / (60 * 1000))
+  if (minutes > 0) {
+    diff += minutes + '分'
+  } else {
+    if (diff !== '') {
+      diff += minutes + '分'
+    }
+  }
+  // 计算相差秒数
+  var leave3 = leave2 % (60 * 1000)
+  var seconds = Math.round(leave3 / 1000)
+  if (seconds > 0) {
+    diff += seconds + '秒'
+  } else {
+    if (diff !== '') {
+      diff += seconds + '秒'
+    } else {
+      // modify by wangnzhenjia
+      // 当创建后立即查看显示
+      diff = '0秒'
+    }
+  }
+
+  return diff
+}
